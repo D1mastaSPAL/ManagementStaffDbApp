d672f342e97f2f0ddf3c4b85491a029f1b259933
D1mastaSPAL <su66071n@mail.ru>
2025-03-16 00:07:20 +0300

    first commit

---
+++ ManagementStaffDbApp/Models/DataWorker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementStaffDbApp.Model;
+
+public static class DataWorker
+{
+    //Создать отдел 
+
+    //Создать позицию
+
+    //Создать сотрудника
+
+    //Удаление отдела
+
+    //Удаление позиции
+
+    //Удаление сотрудника
+
+    //редактирование отдела
+
+    //редактирование позиции
+
+    //редактирование сотрудника
+}
---
+++ ManagementStaffDbApp/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementStaffDbApp.Model;
+
+public class User
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Surname { get; set; }
+
+    public string Phone { get; set; }
+
+    public int PositionId { get; set; }
+
+    public virtual Position Position { get; set; }
+}
